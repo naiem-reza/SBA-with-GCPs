2988d653a1a4b76689aa375cf32920c1180af35c
Reza Naiemaee <109411297+naiem-reza@users.noreply.github.com>
2022-12-12 15:54:37 +0330

    Update SBA.py

--- SBA.py
+++ SBA.py
@@ -2,10 +2,9 @@
 # Sparse-Bundle-Adjustments-with-GCPs
 This python implementation of Sparse bundle adjustment based on the sparse Levenberg-Marquardt algorithm with Ground Control Points (GCPs).
 A bundle adjusmtent problem arises in 3-D reconstruction and it can be formulated as follows (taken from https://en.wikipedia.org/wiki/Bundle_adjustment):
-To derive accurate 3D geospatial information from imagery, it is necessary to establish the camera's interior and exterior orientation parameters.
-Interior orientation parameters (IOPs), which contain the internal sensor elements such as principal distance, principal point coordinates, and lens distortions, are specified through a camera calibration procedure.
+To derive accurate 3D geospatial information from imagery, it is necessary to establish the exterior orientation parameters.
 EOPs, which define the position and orientation of the camera at the point of exposure in a mapping frame, can be established using either Ground Control Points (GCPs) through a Aerial triangulation (AT) process.
-AT is one of the most critical steps in aerial photogrammetry to estimate the Tie points Object coordinates (OC), EOPs, and  IOP which is performed with the bundle Adjustment (BA).
+AT is one of the most critical steps in aerial photogrammetry to estimate the Tie points Object coordinates (OC) and EOPs which is performed with the bundle Adjustment (BA).
 The basic photogrammetric principal geometry consists of three geometric entities: object space points (3D points),
 corresponding image points (2D points), and perspective centers. Such a geometry can be formulized with collinearity equations.
  R_{ij} (i = 1,2,3,... , j = 1,2,3,...) stands for the nine elements of the rotation matrix , which can be modeled by three rotation Euler angles omega,
@@ -18,22 +17,9 @@ R = r_{11} & r_{12} & r_{13}
     [S]  = R . [Y - Y0]
     [Q]        [Z - Z0]
 
-X_0,Y_0 and Z_0 are the translation parameters for the camera station. Meanwhile, X,Y and Z are the object point coordinates usually given in meters in the mapping reference system (i.e., Earth-fixed coordinate system, in the case UTM)
-The additional parameters related to lens distortions, coordinates of the principal point (i.e., the point closest to the projection center),
-and sensor distortions can, in practice, be used to recover the theoretical collinearity condition between image points, camera position, and object point.
-To estimate the 6 EOPs, the image coordinates are first rectifed using the calculated IOPs of the digital camera, consisting of the principal point (c_x,c_x) (in pixel), the focal length f (in pixel),
-the coefcients of radial distortion (K_1, K_2,K_3) (in pixel) and the coefcients of decentring distortion (P1,P2) (pixel) and affinity and non-orthogonality (skew) coefficients (B1, B2) (in pixels).
-According to equations, the image measurements (u, v) (pixel) are rectifed to (u', v') (pixel) according to
+X_0,Y_0 and Z_0 are the translation parameters for the camera station. 
+Meanwhile, X,Y and Z are the object point coordinates usually given in meters in the mapping reference system (i.e., Earth-fixed coordinate system, in the case UTM)
 
-x = - P / Q
-y = S / Q
-r= sqrt{x^2+y^2}
-x' = x(K_1r^2 + K_2r^4 + K_3r^6) + (P_1(r^2+2x^2) + 2P_2xy)
-y' = y(K_1r^2 + K_2r^4 + K_3r^6) + (P_2(r^2+2y^2) + 2P_1xy)
-u' = 0.5w + c_x + x'f + x'B1 + y'B2
-v' = 0.5h + c_y + y'f
-f_{x} = u - u'
-f_{y} = v - v'
 """
 
 
